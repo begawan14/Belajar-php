fc03c6a32f4faffe4725f7791834bc174271c625
begawan14 <24430275_begawan_s_b@widyadharma.ac.id>
2026-09-02 19:57:02 +0700

    versi 3

--- index.php
+++ index.php
@@ -9,24 +9,59 @@
     <?php 
     /*$nama = "Begiiiii";
     $umur = "18";
-    $belummenikah = true;
+    $belummenikah = true;*/
     
-    echo "Hello nama saya $nama, umur saya $umur tahun.";
+    /*echo "Hello nama saya $nama, umur saya $umur tahun.";
     if ($belummenikah == false) {
         "belum nikah";
     } 
     else {
         echo "sudah nikah";
-    }
-    for ($i=1; $i < 10; $i++) {
+    }*/
+    /*for ($i=1; $i < 10; $i++) {
         echo "<br>";
         echo $i;
-    }*/
+    }
 
     for ($i=1; $i < 10; $i+=2) {
         echo $i;
+    }*/
+
+    /*$belanja = 150000;
+
+    if ($belanja > 100000) {
+        $diskon = $belanja * 0.10;
+        $total = $belanja - $diskon;
+        echo "Selamat woe! anda mendapatkan diskon. Bayar: " . $total; 
+    }
+    else{
+        echo "total bayar: " . $belanja;
+    }*/
+    
+    /*$user = "Begi ganteng Sekali";
+    $pass = "kepo banget";
+
+    $input_user = "Begi ganteng Sekali";
+    $input_pass = "kepo banget";
+
+    if ($input_user == $user && $input_pass == $pass){
+        echo "Akses Diterima!";
+    }
+    else{
+        echo "Akses Ditolak!";
+    }*/
+
+    /*$mhs=['April','Vano','Begi'];
+    $datamhs=["nama"=>"Begi","umur"=>18,"Kelas"=>"5A","Prodi"=>"BD"];
+    echo "nama saya $datamhs[nama], umur saya $datamhs[umur], saya kelas $datamhs[Kelas], dengan prodi $datamhs[Prodi]";*/
+    
+    /*function perkalian($angka1, $angka2){
+        return $angka1*$angka2;
     }
 
+    $hasil = perkalian(10,20);
+    echo $hasil; 
+    */
     ?>
     
 </body>
