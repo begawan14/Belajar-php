3bf402337bb4b60a27b5dd2fdc64d4c573f09d24
begawan14 <24430275_begawan_s_b@widyadharma.ac.id>
2026-09-02 19:21:53 +0700

    versi 2

--- index.php
+++ index.php
@@ -6,6 +6,28 @@
     <title>Belajar PHP</title>
 </head>
 <body>
+    <?php 
+    /*$nama = "Begiiiii";
+    $umur = "18";
+    $belummenikah = true;
+    
+    echo "Hello nama saya $nama, umur saya $umur tahun.";
+    if ($belummenikah == false) {
+        "belum nikah";
+    } 
+    else {
+        echo "sudah nikah";
+    }
+    for ($i=1; $i < 10; $i++) {
+        echo "<br>";
+        echo $i;
+    }*/
+
+    for ($i=1; $i < 10; $i+=2) {
+        echo $i;
+    }
+
+    ?>
     
 </body>
 </html>
